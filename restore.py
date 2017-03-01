df5044e36b61f3428f333b92f90341839682ef23
Oliver Sherouse <oliver@oliversherouse.com>
2017-02-28 20:56:57 -0500

    updated restore script

--- restore.py
+++ restore.py
@@ -2,40 +2,46 @@
 
 import logging
 import os
+import re
 import sys
+import shutil
 
-IGNORE_DIRS = ".git",
-IGNORE_FILES = ".gitignore", "README"
+from pathlib import Path
+
+IGNORES = (
+        '.git/',
+        '.gitignore',
+        'README',
+        'restore.py'
+        )
+
+RES = tuple(re.compile(i) for i in IGNORES)
 
 logging.basicConfig(level=logging.INFO)
 
 
 def link(target, source):
     logging.info("Restoring from {0} to {1}".format(source, target))
-    if os.path.islink(target):
+    if target.is_symlink():
         logging.info("Deleting existing link")
-        os.remove(target)
-    if os.path.exists(target):
-        backup = "{0}.bak".format(target)
+        target.unlink()
+    if target.exists():
+        backup = Path(str(target) + '.bak')
+        target.rename(backup)
         logging.info("Backing up existing {0} to {1}".format(target, backup))
-    os.symlink(source, target)
+    target.symlink_to(source, source.is_dir())
 
 
 def main():
     """
     Restore entire current directory to home directory
     """
-    target = os.environ['HOME']
-    source = os.path.abspath(os.path.dirname(sys.argv[0]))
-    for name in os.listdir(source):
-        sourcename = os.path.join(source, name)
-        if os.path.abspath(sourcename) == os.path.abspath(sys.argv[0]):
-            continue  # Don't Copy Self
-        targetname = os.path.join(target, "." + name)
-        if os.path.isdir(sourcename) and name not in IGNORE_DIRS:
-            link(targetname, sourcename)
-        elif os.path.isfile(sourcename) and name not in IGNORE_FILES:
-            link(targetname, sourcename)
+    target = Path(os.environ['HOME']).resolve()
+    source = Path(__file__).resolve().parent
+    for subpath in source.iterdir():
+        if not any(pattern.search(str(subpath)) for pattern in RES):
+            link(target.joinpath('.' + subpath.name), subpath)
+
 
 if __name__ == "__main__":
     main()
