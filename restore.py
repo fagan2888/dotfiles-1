e955dca61158cde4d126962afc178de9cd6c7a04
Oliver Sherouse <oliver@oliversherouse.com>
2019-12-08 14:18:31 -0500

    finally developed common sense, moved actual files to subdir

--- restore.py
+++ restore.py
@@ -2,13 +2,8 @@
 
 import logging
 import os
-import re
-import sys
-import shutil
 
 from pathlib import Path
-
-IGNORES = {'.git', '.gitignore', 'README', 'restore.py'}
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,7 +14,7 @@ def link(target, source):
         logging.info("Deleting existing link")
         target.unlink()
     if target.exists():
-        backup = Path(str(target) + '.bak')
+        backup = Path(str(target) + ".bak")
         target.rename(backup)
         logging.info("Backing up existing {0} to {1}".format(target, backup))
     target.symlink_to(source, source.is_dir())
@@ -29,11 +24,10 @@ def main():
     """
     Restore entire current directory to home directory
     """
-    target = Path(os.environ['HOME']).resolve()
-    source = Path(__file__).resolve().parent
+    target = Path(os.environ["HOME"]).resolve()
+    source = Path(__file__).resolve().parent.joinpath("dotfiles")
     for subpath in source.iterdir():
-        if not subpath in IGNORES:
-            link(target.joinpath('.' + subpath.name), subpath)
+        link(target.joinpath("." + subpath.name), subpath)
 
 
 if __name__ == "__main__":
